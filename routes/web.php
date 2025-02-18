e88a459adc7199ab9785a63051a2d1c9e90e3241
dantrif <dan.trif@ddroidd.com>
2025-02-18 16:27:05 +0200

    Move reports page to admin group Add README.md plus screenshots

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,15 @@
 
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\VisitController;
-use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/report', [VisitController::class, 'showReport'])->name('report');
-
 Route::group(['prefix' => 'admin'],function (){
+    Route::get('/report', [VisitController::class, 'showReport'])->name('report');
+
     Route::group(['prefix' => 'clients'],function (){
         Route::get('', [ClientController::class, 'index'])->name('clients.index');
         Route::post('', [ClientController::class, 'store'])->name('clients.store');
